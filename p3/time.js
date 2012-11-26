7086ea162b383cff890e71e63dac20419c754b56
Daisy Laflamme <otli4ni4ka@yahoo.com>
2012-11-25 22:45:39 -0500

    p3 second commit

--- p3/time.js
+++ p3/time.js
@@ -1,49 +1,50 @@
 $(document).ready(function() {
-var work = 5000;
-var t1;
-var alarm;
+
+var work = 2400000; //40 minutes of work
+var t1; // represents the time interval of work or break 
+var alarm; // audio sound 
+//initialize and start playing the alarm
 var alarmStart = function () {
-	/* alarm */
 	alarm = document.getElementById("alarm");
 	if (alarm == null) alert("Failed to initialize alarm.");
 	//alarm.load();
 	alarm.play();
 }
+//setting the time for 40 minutes,then playing the alarm and giving alert of choice
  $('.start').click(function(event){
 	t1 = window.setTimeout(function() {
     alarmStart();	
 	alert("Press Break or Continue button!");
 	
-		}, 5000);	
+		}, work);	
 	})
-
+//reseting the set time of work or break
 $('#pause').click(function(event) {
 	 clearTimeout(t1);	
 	});	
-	
+//stop counting the time of work or break
 $('#reset').click(function(event) {
 	 clearTimeout(t1);	
 	});	
-	
+//setting interval of 10 minutes break, then playing the alarm, alerting for break end, and setting the work time for 40 minutes	
 $('#break').click(function(event) {
-	var rest = 2000;
-	window.setTimeout(function() {
+	var rest = 600000;
+	t1 = window.setTimeout(function() {
     	alarmStart();
-		alert("Time to work!");
+		alert("End of Break!");
 		
 		window.setTimeout(function() {
     	alarmStart();
-		alert("Your time is up!");
-		}, 5000);	
+		alert("Press Break or Continue button!");
+		}, work);	
 	}, rest);	
 });
-
+/*
 $('#sound_stop').click(function(event){
 	  $('#alarm').stop();	
 });
 	
-
-/*var d = new Date();
+var d = new Date();
 console.log(d.getSeconds());	
 var last, diff;
 $('div').click(function(event) {
